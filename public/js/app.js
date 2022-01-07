5d0cbdfc6c4033bdddb762994c3e7c165342a49c
Edwin Dominguez <edwindominguez@hotmail.com>
2022-01-06 22:09:52 -0500

    preparing for deployment

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,10 @@ const search = document.querySelector('#location')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-// messageOne.textContent = 'From JavaScript'
-
 weatherForm.addEventListener('submit', e => {
   e.preventDefault()
   const location = search.value
-  const url = 'http://localhost:3000/weather?address=' + location
+  const url = '/weather?address=' + location
   messageOne.textContent = 'Loading...'
   messageTwo.textContent = ''
   fetchWeather(url)
